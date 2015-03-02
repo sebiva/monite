18546b21cc1d63fd80a80e5053853be6e4a0bc0f
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2015-03-02 16:40:26 +0100

    Add skeleton for eval. Fix list for ListEl in grammar.
    Eval can send basic strings directly to createProcess and proc

--- lab3/src/Monite/Interpret.hs
+++ lab3/src/Monite/Interpret.hs
@@ -6,7 +6,11 @@ where
 import Grammar.ErrM (Err (..) )
 import Grammar.Par (pProgram, myLexer)
 import Grammar.Print (printTree)
-import Grammar.Abs (Exp, Lit, Cmd, Program)
+import Grammar.Abs
+
+import System.Process
+
+import Data.List (intersperse)
 
 -- | The main loop which interprets the shell commands executed by the user
 interpret :: String -> IO ()
@@ -23,4 +27,44 @@ interpret s = do
 
 -- | Evaluate the abstract syntax tree, as parsed by the lexer
 eval :: Program -> IO ()
-eval p = return ()
+eval (PProg exps) = mapM_ evalExp exps
+
+evalExp :: Exp -> IO ()
+evalExp e = case e of
+  (EComp e1 v e2)   -> undefined
+  (ELet v e1 e2)    -> undefined
+  (EList els)       -> undefined
+  (ECmd c)          -> do
+    (s:ss) <- evalCmd c
+    (_,_,_,p) <- createProcess (proc s ss)
+    waitForProcess p
+    return ()
+
+
+evalCmd :: Cmd -> IO ([String]) -- String for testing
+evalCmd c = case c of
+  (CText ts)        -> return $ concatText ts
+  (CPipe c1 c2)     -> undefined
+  (COut c1 c2)      -> undefined
+  (CIn c1 c2)       -> undefined
+
+concatText :: [Text] -> [String]
+concatText ts = map get ts
+  where get (TLit (Lit s)) = s
+        get (TVar (Var s)) = s
+{-data ListEl =-}
+   {-LExp Exp-}
+  {-deriving (Eq,Ord,Show,Read)-}
+
+{-data Cmd =-}
+   {-CText [Text]-}
+ {-| CPipe Cmd Cmd-}
+ {-| COut Cmd Cmd-}
+ {-| CIn Cmd Cmd-}
+  {-deriving (Eq,Ord,Show,Read)-}
+
+{-data Text =-}
+   {-TLit Lit-}
+ {-| TVar Var-}
+  {-deriving (Eq,Ord,Show,Read)-}
+

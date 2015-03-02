1d968bfe0a89242a92059bfb7a653165b6b500ac
John Martinsson <john.martinsson@gmail.com>
2015-03-02 17:48:34 +0100

    Adding the MoniteM monad, which handles state and error.

--- lab3/src/Monite/Interpret.hs
+++ lab3/src/Monite/Interpret.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Monite.Interpret (
     interpret     -- :: String -> IO ()
   )
@@ -8,9 +9,32 @@ import Grammar.Par (pProgram, myLexer)
 import Grammar.Print (printTree)
 import Grammar.Abs
 
-import System.Process
+import System.Process (createProcess, waitForProcess, proc )
+
+import Control.Monad ( Monad )
+import Control.Applicative ( Applicative )
+import Control.Monad.Except ( ExceptT, runExceptT )
+import Control.Monad.State.Lazy ( MonadState, StateT, evalStateT )
+import Control.Monad.IO.Class ( liftIO, MonadIO )
 
 import Data.List (intersperse)
+import qualified Data.Map as M
+
+newtype MoniteM a = Monite { runMonite :: StateT Env
+                                            (ExceptT MoniteErr IO) a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadState Env)
+
+data Env = Env {
+  vars :: M.Map Var String,
+  path :: FilePath
+}
+
+data MoniteErr = Err {
+  errPath :: FilePath,
+  errCmd  :: String,
+  errMsg  :: String
+}
+
 
 -- | The main loop which interprets the shell commands executed by the user
 interpret :: String -> IO ()
@@ -21,27 +45,35 @@ interpret s = do
       putStrLn (show tree)    -- TODO : Debug
       putStrLn "--------------------------"
       putStrLn (printTree tree)
-      eval tree               -- Evaluate the tree
+
+      res <- runExceptT $ evalStateT (runMonite (eval tree)) emptyEnv
+      case res of
+        Left err -> putStrLn "error"
+        Right a  -> return ()
+
     Bad err -> do
       putStrLn err
 
+emptyEnv :: Env
+emptyEnv = Env M.empty "." -- -- TODO: $HOME? / Conf : 2015-03-02 - 17:28:51 (John)
+
 -- | Evaluate the abstract syntax tree, as parsed by the lexer
-eval :: Program -> IO ()
+eval :: Program -> MoniteM ()
 eval (PProg exps) = mapM_ evalExp exps
 
-evalExp :: Exp -> IO ()
+evalExp :: Exp -> MoniteM ()
 evalExp e = case e of
   (EComp e1 v e2)   -> undefined
   (ELet v e1 e2)    -> undefined
   (EList els)       -> undefined
   (ECmd c)          -> do
-    (s:ss) <- evalCmd c
-    (_,_,_,p) <- createProcess (proc s ss)
-    waitForProcess p
+    (s:ss)    <- evalCmd c
+    (_,_,_,p) <- liftIO $ createProcess (proc s ss)
+    liftIO $ waitForProcess p
     return ()
 
 
-evalCmd :: Cmd -> IO ([String]) -- String for testing
+evalCmd :: Cmd -> MoniteM ([String]) -- String for testing
 evalCmd c = case c of
   (CText ts)        -> return $ concatText ts
   (CPipe c1 c2)     -> undefined

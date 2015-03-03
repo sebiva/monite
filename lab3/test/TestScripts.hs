fe030c6b4e0afceac9de9ae7918b422e659ccd78
John Martinsson <john.martinsson@gmail.com>
2015-03-03 21:19:25 +0100

    Fixed the test suite, had to wait for the process to finish. It is cumbersome to run the suite as it is now, one must remove the directory test/files after each run, and it will not pass if not all putStrLn output is surpressed in Monite.Interpret.

--- lab3/test/TestScripts.hs
+++ lab3/test/TestScripts.hs
@@ -9,20 +9,20 @@ import Control.Monad.IO.Class
 
 main :: IO ()
 main = do
-  putStrLn "Hello, this failed"
   (fp, h) <- openTempFile "." ".temp"
-  p <- createProcess $ process "./test/good/good.sh" (UseHandle h)
+  (_, _, _, p) <- createProcess $ process "./test/good/good.sh" (UseHandle h)
+  waitForProcess p
   h1 <- openFile fp ReadMode
   output <- hGetContents h1 -- TODO: Does not seem to read the file : 2015-03-03 - 11:12:37 (John)
-  putStrLn output
+  putStr output
   eOutput <- readFile $ "./test/good/good.sh.output"
-  putStrLn eOutput
+  putStr eOutput
   removeFile fp
   if output == eOutput then return ()
   else exitFailure
 
 process :: FilePath -> StdStream -> CreateProcess
-process t output = CreateProcess { cmdspec = RawCommand "dist/build/monite/monite" [t],
+process t output = CreateProcess { cmdspec = RawCommand "./dist/build/monite/monite" [t],
                           cwd     = Nothing,
                           env     = Nothing,
                           std_in  = Inherit,
@@ -31,5 +31,3 @@ process t output = CreateProcess { cmdspec = RawCommand "dist/build/monite/monit
                           close_fds = False,
                           create_group = False,
                           delegate_ctlc = False}
-
-
--- lab3/test/good/good.sh
+++ lab3/test/good/good.sh
@@ -1,1 +1,8 @@
-ls ;
+cd test/ ;                       # test dir
+mkdir files;
+cd files/ ;
+touch test1;
+ls ;                            # stdout: test1
+echo piped > pipe;              # files: test1 pipe
+cat pipe;                       # stdout: piped
+ls ;                            # stdout: test1 piped

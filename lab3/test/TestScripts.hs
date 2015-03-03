4b115249bb8ad465ff3fdd60a5dd8b28fc92f5e6
John Martinsson <john.martinsson@gmail.com>
2015-03-03 11:13:35 +0100

    Writing a more comprehensive test suite, does not work as intended yet. The output of the monite binary does get catched by the handle file, however, it does not seem to be possible to read it again...

--- lab3/test/TestScripts.hs
+++ lab3/test/TestScripts.hs
@@ -1,8 +1,35 @@
 module Main (main) where
 
 import Monite.Interpret
+import System.Process
+import System.IO
+import System.Exit
+import System.Directory
+import Control.Monad.IO.Class
 
 main :: IO ()
 main = do
-  s <- readFile "test/Test.sh" -- TODO: Dynamic file placement? : 2015-03-02 - 14:43:00 (John)
-  interpret s                  -- Interpret the test file
+  putStrLn "Hello, this failed"
+  (fp, h) <- openTempFile "." ".temp"
+  p <- createProcess $ process "./test/good/good.sh" (UseHandle h)
+  h1 <- openFile fp ReadMode
+  output <- hGetContents h1 -- TODO: Does not seem to read the file : 2015-03-03 - 11:12:37 (John)
+  putStrLn output
+  eOutput <- readFile $ "./test/good/good.sh.output"
+  putStrLn eOutput
+  rmoveFile fp
+  if output == eOutput then return ()
+  else exitFailure
+
+process :: FilePath -> StdStream -> CreateProcess
+process t output = CreateProcess { cmdspec = RawCommand "dist/build/monite/monite" [t],
+                          cwd     = Nothing,
+                          env     = Nothing,
+                          std_in  = Inherit,
+                          std_out = output,
+                          std_err = Inherit,
+                          close_fds = False,
+                          create_group = False,
+                          delegate_ctlc = False}
+
+

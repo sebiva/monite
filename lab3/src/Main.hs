9c160ac2e0b9b76daf5070e699bfcf4c8d9956c3
John Martinsson <john.martinsson@gmail.com>
2015-03-03 15:57:04 +0100

    Adding run, and emptyEnv to Main. We needed to run the MoniteM monad in the loop, rather than in the interpert function, as to perserve the state of the monad.

--- lab3/src/Main.hs
+++ lab3/src/Main.hs
@@ -2,27 +2,50 @@ module Main (main) where
 
 import System.Environment (getArgs)
 import System.Console.Haskeline
-import Monite.Interpret (interpret)
+import System.Directory ( getHomeDirectory, removeFile )
+
+import Monite.Interpret
+
 import Control.Monad.IO.Class (liftIO)
+{-import Control.Monad ( Monad )-}
+{-import Control.Applicative ( Applicative )-}
+import Control.Monad.Except ( ExceptT, runExceptT )
+import Control.Monad.State.Lazy ( MonadState, StateT, evalStateT, get, modify, lift )
+import Control.Monad.IO.Class ( liftIO, MonadIO )
+
+import qualified Data.Map as M
 
 -- | Starting the shell main loop with a possible script file as argument.
 main :: IO ()
 main = do
   args <- getArgs
+  env <- emptyEnv
   case args of
     [file] -> do s <- readFile file               -- read the script file
-                 interpret s                      -- interpret the script file
-    _      -> getInput                            -- get user commands
+                 run (interpret s) env            -- interpret the script file
+                 return ()
+    _      -> do getInput env                     -- get user commands
 
 -- | Get commands from the user
-getInput :: IO ()
-getInput = runInputT defaultSettings loop         -- input loop w/ default sets
+getInput :: Env -> IO ()
+getInput env = runInputT defaultSettings (loop env)         -- input loop w/ default sets
   where
-    loop :: InputT IO ()
-    loop = do
+    loop :: Env -> InputT IO ()
+    loop env = do
       minput <- getInputLine "% "                 -- get user command
       case minput of
         Nothing -> return ()                      -- nothing entered
         Just "quit" -> return ()                  -- quit the shell loop
-        Just input -> do liftIO $ interpret input -- interpret user command
-                         loop
+        Just input -> do env <- liftIO $ run (interpret input) env -- interpret user command
+                         loop env
+
+run :: MoniteM a -> Env -> IO a
+run m env = do
+  res <- runExceptT $ evalStateT (runMonite m) env
+  case res of
+    Left err -> fail $ "error"
+    Right a  -> return a
+
+emptyEnv :: IO Env
+emptyEnv = do home <- getHomeDirectory
+              return $ Env M.empty home

425e7112c9b24055d96289560aa920d7a7990b33
John Martinsson <john.martinsson@gmail.com>
2015-03-06 18:51:30 +0100

    I need to put my glasses on. Now the historyfile should be placed in the home directory.

--- lab3/src/Main.hs
+++ lab3/src/Main.hs
@@ -32,7 +32,7 @@ main = do
 -- | Get commands from the user
 inputLoop :: Env -> IO ()
 inputLoop env = do
-  path <- getCurrentDirectory
+  path <- getHomeDirectory
   runInputT (mySettings path) (loop env)         -- input loop w/ default sets
   where
     loop :: Env -> InputT IO ()

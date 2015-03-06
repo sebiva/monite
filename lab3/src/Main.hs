f4ff45808b647795af3235618b22cb6a0f85c02e
John Martinsson <john.martinsson@gmail.com>
2015-03-06 18:46:45 +0100

    The previous historyfile seemed to be placed in every folder monite was executed in. My bad. It is now placed in the home directory
      ~/monitehistory

--- lab3/src/Main.hs
+++ lab3/src/Main.hs
@@ -13,8 +13,8 @@ import Control.Monad.IO.Class ( liftIO, MonadIO )
 import qualified Data.Map as M
 
 -- TODO: Fix file placement! : 2015-03-06 - 16:08:11 (John)
-mySettings :: Settings IO
-mySettings = defaultSettings { historyFile = Just "monitehistory" }
+mySettings :: FilePath -> Settings IO
+mySettings path = defaultSettings { historyFile = Just $ path ++ "/monitehistory" }
 
 -- | Starting the shell main loop with a possible script file as argument.
 main :: IO ()
@@ -31,7 +31,9 @@ main = do
 
 -- | Get commands from the user
 inputLoop :: Env -> IO ()
-inputLoop env = runInputT mySettings (loop env)         -- input loop w/ default sets
+inputLoop env = do
+  path <- getCurrentDirectory
+  runInputT (mySettings path) (loop env)         -- input loop w/ default sets
   where
     loop :: Env -> InputT IO ()
     loop env = do

d10e9a9fd1827eb5ae7a5145dcb5741111a4df33
John Martinsson <john.martinsson@gmail.com>
2015-03-09 23:05:07 +0100

    Adding a new exposed module Monite.Test, used in the test suites.
    The makefile now compiles the grammar test file again.
    
    The test-scripts, and test-grammars test suites now test all the files in the
    test script directories, and checks their output against the expected output.
    
    TODO: The test-grammars suite does not have proper tests in it yet, we need to
    figure out the grammars first, and then write what we would expect as output.
    
    Test are run simply by:
    
    -------------------------------------------------------------------------------
    test-scripts:
    -------------------------------------------------------------------------------
    > cabal configure --enable-tests; and cabal build; and cabal test-scripts
    
    -------------------------------------------------------------------------------
    test-grammars:
    -------------------------------------------------------------------------------
    > cd src; and make  # Too make sure the latest grammars are used for the tests.
    > cabal configure --enable-tests; and cabal build; and cabal test-grammars
    
    -------------------------------------------------------------------------------
    all-tests:
    -------------------------------------------------------------------------------
    > cabal configure --enable-tests; and cabal build; and cabal test

---
+++ lab3/src/Monite/Test.hs
@@ -0,0 +1,65 @@
+module Monite.Test (
+    runTests
+  , runTest
+  , process
+
+) where
+
+import Monite.Interpret
+import System.Process
+import System.IO
+import System.Exit (exitFailure)
+import System.Directory (setCurrentDirectory, getDirectoryContents, removeFile)
+import Control.Monad.IO.Class
+import Control.Monad (foldM)
+import Data.List (isSuffixOf)
+
+-- | Run all tests using the supplied binary, and the directory to the test
+-- files
+runTests :: FilePath -> FilePath -> IO ()
+runTests bin dirTests = do
+  -- Get all files in the test directory
+  files <- getDirectoryContents dirTests
+  -- Filter out the scripts, and create their relative paths
+  let scripts = map (dirTests ++) $ filter (".sh" `isSuffixOf`) files
+  -- Test all the scripts
+  passed <- foldM test True scripts
+  if passed then return () else exitFailure
+  where
+    test :: Bool -> FilePath -> IO Bool
+    test b path = do
+      passed <- runTest bin path
+      return $ b && passed
+
+-- | Run a test scripts, and check if the output is the same as the expected
+-- output
+runTest :: FilePath -> FilePath -> IO Bool
+runTest bin path = do
+  (fp, h) <- openTempFile "." ".temp"
+  (_, _, _, p) <- createProcess $ process bin path (UseHandle h)
+  waitForProcess p
+  h1 <- openFile fp ReadMode
+  output <- hGetContents h1
+  putStr output
+  eOutput <- readFile $ (path ++ ".output")
+  putStr eOutput
+  removeFile fp
+  if output == eOutput then return True
+  else do putStrLn ("----------------------------------------------------")
+          putStrLn ("-- Testfile: " ++ path ++ " failed")
+          putStrLn ("----------------------------------------------------")
+          putStrLn ("Output:\n" ++ output)
+          putStrLn ("Expected:\n" ++ eOutput)
+          return False
+
+-- | Spawn a process with the moniteshell running a script file
+process :: FilePath -> FilePath -> StdStream -> CreateProcess
+process bin t output = CreateProcess { cmdspec = RawCommand bin [t],
+                          cwd     = Nothing,
+                          env     = Nothing,
+                          std_in  = Inherit,
+                          std_out = output,
+                          std_err = Inherit,
+                          close_fds = False,
+                          create_group = False,
+                          delegate_ctlc = False}
